13dee48a97ee1918e9eb7bf71bfad13a559a5cc4
Evgeny Vrublevsky <me@veg.by>
2017-12-09 15:39:10 +0300

    Port to VS2017

---
+++ RuntimeHacks.cpp
@@ -0,0 +1,14 @@
+// "?terminate@@YAXXZ" from "msvcrt.dll" will be used
+void __cdecl terminate(void);
+
+// Helper function referenced by the front-end to assist in implementing noexcept.
+extern "C" __declspec(noreturn) void __cdecl __std_terminate()
+{
+	terminate();
+}
+
+// Fallbacks the scalar operator delete with size_t
+void __cdecl operator delete(void* const block, size_t const) noexcept
+{
+	operator delete(block);
+}
