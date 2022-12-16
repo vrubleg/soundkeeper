7d46b32e8a36de06d90e514a4e8229db355c582c
Evgeny Vrublevsky <me@veg.by>
2022-12-16 17:11:10 +0200

    Call csi.exe by absolute path to not rely on PATH.

--- BuildInfo.hpp
+++ BuildInfo.hpp
@@ -9,12 +9,12 @@
 #define REV_MAJOR       1
 #define REV_MINOR       3
 #define REV_PATCH       0
-#define REV_EXTRA       11
+#define REV_EXTRA       12
 #define REV_BUILD       2
 
 #define REV_YEAR        2022
 #define REV_MONTH       12
 #define REV_DAY         16
-#define REV_DAY_BUILD   2
+#define REV_DAY_BUILD   4
 
 #endif // BUILDINFO_HPP
