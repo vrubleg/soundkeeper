8d8c2e17a847bb20fca55a3395cee73f74d19fb1
Evgeny Vrublevsky <me@veg.by>
2023-05-13 20:46:01 +0300

    Compiler also knows that we use NODEFAULTLIB in Release.

--- RuntimeHacks.cpp
+++ RuntimeHacks.cpp
@@ -1,7 +1,7 @@
 // We use msvcrt.dll as a C runtime to save space in release builds.
 // It has limited set of functions, so we reimplement missing ones here.
 
-#if !defined(_CONSOLE) && _MSC_VER >= 1900 /* VS2015+ */
+#if defined(_VC_NODEFAULTLIB) && _MSC_VER >= 1900 /* VS2015+ */
 
 // Fallback for the sized delete (introduced in C++14).
 void __cdecl operator delete(void* const block, size_t const) noexcept
