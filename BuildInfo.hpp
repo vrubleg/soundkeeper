03c122dc5d50648d58c60a080178a29f03f4d715
Evgeny Vrublevsky <me@veg.by>
2023-07-21 22:42:35 +0300

    Merge bogus _RDATA section (that comes from CRT) into normal .rdata.

--- BuildInfo.hpp
+++ BuildInfo.hpp
@@ -9,12 +9,12 @@
 #define REV_MAJOR       1
 #define REV_MINOR       3
 #define REV_PATCH       1
-#define REV_EXTRA       8
-#define REV_BUILD       2
+#define REV_EXTRA       9
+#define REV_BUILD       4
 
 #define REV_YEAR        2023
 #define REV_MONTH       7
 #define REV_DAY         21
-#define REV_DAY_BUILD   13
+#define REV_DAY_BUILD   17
 
 #endif // BUILDINFO_HPP
