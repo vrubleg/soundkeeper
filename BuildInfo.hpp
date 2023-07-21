c11f2eb1dbd7469769ea80268ba3426bc533fcad
Evgeny Vrublevsky <me@veg.by>
2023-07-21 22:33:33 +0300

    Strip .reloc and merge .pdata into .rdata sections.

--- BuildInfo.hpp
+++ BuildInfo.hpp
@@ -9,12 +9,12 @@
 #define REV_MAJOR       1
 #define REV_MINOR       3
 #define REV_PATCH       1
-#define REV_EXTRA       7
-#define REV_BUILD       8
+#define REV_EXTRA       8
+#define REV_BUILD       2
 
 #define REV_YEAR        2023
 #define REV_MONTH       7
 #define REV_DAY         21
-#define REV_DAY_BUILD   11
+#define REV_DAY_BUILD   13
 
 #endif // BUILDINFO_HPP
