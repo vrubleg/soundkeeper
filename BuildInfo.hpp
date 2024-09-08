45d70a76ad23ef3cab7e12fc2acc53b2e1e98a48
Evgeny Vrublevsky <me@veg.by>
2024-09-08 20:03:54 +0300

    Enable multi-processor compilation.

--- BuildInfo.hpp
+++ BuildInfo.hpp
@@ -9,12 +9,12 @@
 #define REV_MAJOR       1
 #define REV_MINOR       3
 #define REV_PATCH       3
-#define REV_EXTRA       12
+#define REV_EXTRA       13
 #define REV_BUILD       3
 
 #define REV_YEAR        2024
 #define REV_MONTH       9
-#define REV_DAY         7
+#define REV_DAY         8
 #define REV_DAY_BUILD   3
 
 #endif // BUILDINFO_HPP
